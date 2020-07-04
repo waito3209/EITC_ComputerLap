1286e81ced75219d412d7fa14b0c0927c5953c18
-mwaito3209 <-mwaitotsang@outlook.com>
2020-07-04 10:47:05 +0800

    0704 v2

--- AIV1.py
+++ AIV1.py
@@ -1,10 +1,35 @@
 from support import *
-try:
-    from __future__ import timemachine
-except:
-    pass
+import numpy as np
+import matplotlib.pyplot as plt
+traindatapath=input('traindatafile')
+testdatapath=input('traindatafile')
 
-datalist = list(combinations(listdir(input('traindatafile')), 2))
-detail(datalist)
-for i in list(datalist):
-    print(i)
+datatrainlist = list(combinations(listdir(traindatapath), 2))
+datatestlist = list(combinations(listdir(testdatapath), 2))
+detail(datatrainlist)
+traindata=[]
+trainlabel=[]
+testdata=[]
+testlabel=[]
+for i in list(datatrainlist):
+    p1=np.load(traindatapath + '/'+i[1] )
+    p2=np.load(traindatapath + '/'+i[0] )
+    detail(traindata.append(np.reshape(np.append(p1,p2),300*300*2*3)))
+    if i[1][:3]==i[0][:3]:
+        trainlabel.append(1)
+    else:
+        trainlabel.append(0)
+for i in list(datatestlist):
+    p1=np.load(testdatapath + '/'+i[1] )
+    p2=np.load(testdatapath + '/'+i[0] )
+    detail(testdata.append(np.reshape(np.append(p1,p2),300*300*2*3)))
+    if i[1][:3]==i[0][:3]:
+        testlabel.append(1)
+    else:
+        testlabel.append(0)
+print('Training data shape : ', traindata.shape, trainlabel.shape)
+print('Testing data shape : ', testdata.shape, testlabel.shape)
+from sklearn.preprocessing import LabelBinarizer
+lblbin = LabelBinarizer()
+train_labels_onehot = lblbin.fit_transform(trainlabel)
+test_labels_onehot = lblbin.transform(testlabel)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='EITC_ComputerLap',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='eitc',
+    author_email='',
+    description=''
+)
